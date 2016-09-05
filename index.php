ea21ceaaabe82a73cbab579f2a5df0f7f692aed8
Miyagi Kazuyuki <e16004@std.it-college.ac.jp>
2016-09-05 21:28:29 +0900

    テンプレート作成

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
 <script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <script src="https://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
 <![endif]-->
-<title><?php bloginfo('name'); ?>/</title>
+<title><?php bloginfo('name'); ?></title>
 </head>
-<body class="home">
+<body <?php body_class(); ?>>
     <header class="globalHeader">
         <div class="inner">
-            <h1><a href="/"><img src="<?php echo get_template_directory_uri(); ?>/images/common/logo01.png" height="40" width="300" alt="ホテル・技評リゾート石垣島"></a></h1>
-            <p class="description">サイトのキャッチフレーズ</p>
+            <h1><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/common/logo01.png" height="40" width="300" alt="ホテル・技評リゾート石垣島"></a></h1>
+            <p class="description"><?php bloginfo('description'); ?></p>
             <form action="" method="get" class="searchform">
                 <input type="text" value="" name="s" id="s" placeholder="サイト内検索">
                 <input type="submit" id="searchSubmit" value="">
