32f2bc5e1d5d37aae21e903b1d65206394844922
Miyagi Kazuyuki <e16004@std.it-college.ac.jp>
2016-09-13 21:51:06 +0900

    画像バナーのリンク設定

--- front-page.php
+++ front-page.php
@@ -1,28 +1,27 @@
 <?php get_header(); ?>
-  <div class="contentsWrap">
-      <div class="mainContents">
+<div class="contentsWrap">
+  <div class="mainContents">
 
-          <div class="aboutBlock block">
-              <div class="banners">
-                  <ul>
-                      <li><a href="about.html"><img src="<?php echo get_template_directory_uri(); ?>/images/home/bnr_about.png" height="97" width="320" alt="ホテル紹介"></a></li>
-                      <li><a href="access.html"><img src="<?php echo get_template_directory_uri(); ?>/images/home/bnr_access.png" height="97" width="320" alt="アクセス"></a></li>
-                  </ul>
-              </div>
-          </div><!-- /.aboutBlock -->
+    <div class="aboutBlock block">
+      <div class="banners">
+        <ul>
+          <li><a href="<?php echo get_permalink( 29 ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/home/bnr_about.png" height="97" width="320" alt="ホテル紹介"></a></li>
+          <li><a href="<?php echo get_permalink( 38 ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/home/bnr_access.png" height="97" width="320" alt="アクセス"></a></li>
+        </ul>
+      </div>
+    </div><!-- /.aboutBlock -->
 
-          <section class="newsBlock block">
-              <h1 class="type-B"><span>新着情報</span></h1>
-              <?php get_template_part('loop', 'main'); ?>
-          </section><!-- /.newsBlock -->
+    <section class="newsBlock block">
+      <h1 class="type-B"><span>新着情報</span></h1>
+      <?php get_template_part('loop', 'main'); ?>
+    </section><!-- /.newsBlock -->
+  </div><!-- /.mainContents -->
 
-      </div><!-- /.mainContents -->
-
-      <aside class="subContents">
-      <?php get_sidebar(); ?>
-      <?php get_sidebar('categories'); ?>
-      <?php get_sidebar('archives'); ?>
-      </aside><!-- /.subContents -->
-  </div><!-- /.contentsWrap -->
+  <aside class="subContents">
+    <?php get_sidebar(); ?>
+    <?php get_sidebar('categories'); ?>
+    <?php get_sidebar('archives'); ?>
+  </aside><!-- /.subContents -->
+</div><!-- /.contentsWrap -->
 
 <?php get_footer(); ?>
