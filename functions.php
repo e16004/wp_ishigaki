582c261e2dfbfd6a1064f1ece9f11413539887bf
Miyagi Kazuyuki <e16004@std.it-college.ac.jp>
2016-09-20 20:28:24 +0900

    4-02 RSS出力カスタマイズ
    function.php編集
    function my_excerpt_more    permalink(get_the_ID()) に変更
    RSSにアイキャッチ画像を追加表示
    wp-admin 表示設定 抜粋→全文表示に変更

--- functions.php
+++ functions.php
@@ -9,7 +9,14 @@ add_theme_support( 'post-thumbnails' );
  */
 add_theme_support( 'menus' );
 
-add_filter('comment_form_default_fields', 'my_comment_form_default_fields');
+
+/**
+ * コメント投稿フォームの「名前」「メールアドレス」「ウェブサイト」の項目を削除する
+ */
+add_filter(
+  'comment_form_default_fields',
+  'my_comment_form_default_fields'
+);
 function my_comment_form_default_fields( $args ) {
   $args['author'] = ''; // 「名前」欄を削除
   $args['email'] = ''; // 「メールアドレス」欄を削除
@@ -17,4 +24,27 @@ function my_comment_form_default_fields( $args ) {
   return $args;
 }
 
+//フィードの表示
+add_theme_support( 'automatic-feed-links' );
+add_filter('excerpt_mblength', 'my_excerpt_mblength');
+function my_excerpt_mblength( $length ) {
+  return 50; //抜粋に出力する文字数
+}
+function my_excerpt_more( $more ) {
+  return '...<a href="'. get_permalink( get_the_ID() ) . '">続きを読む→</a>';
+}
+
+//RSSにアイキャッチ画像を追加
+add_filter( 'the_excerpt_rss',  'rss_post_thumbnail');
+add_filter( 'the_content_feed', 'rss_post_thumbnail');
+function rss_post_thumbnail( $content) {
+  global $post;
+  if (has_post_thumbnail( $post->ID)) {
+    $content = '<p>' . get_the_post_thumbnail($post->ID) .'</p>' . $content;
+  }
+  return $content;
+}
+
+//RSS停止  RSS 2.0
+// remove_action('do_feed_rss2', 'do_feed_rss2', 10, 1);
 
