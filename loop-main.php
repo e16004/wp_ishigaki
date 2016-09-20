e7b3bac9ed78e787c5005a567ab18f6d22739fe4
Miyagi Kazuyuki <e16004@std.it-college.ac.jp>
2016-09-20 21:29:49 +0900

    WP_Queryを使用して新しく表示

--- loop-main.php
+++ loop-main.php
@@ -2,26 +2,26 @@
 if ( have_posts() ) :
   while ( have_posts() ) : the_post();
 ?>
-<article id="post-<?php the_ID() ?>" <?php post_class('news'); ?>>
-  <div class="text">
-    <div class="entryInfo">
-      <div class="categories">
-        <?php the_category(); ?>
+  <article id="post-<?php the_ID() ?>" <?php post_class('news'); ?>>
+    <div class="text">
+      <div class="entryInfo">
+        <div class="categories">
+          <?php the_category(); ?>
+        </div>
+        <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y年d月m日(l)'); ?></time>
       </div>
-      <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y年d月m日(l)'); ?></time>
+      <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+      <?php the_excerpt(); ?>
+      <p>[<a href="<?php the_permalink(); ?>">続きを読む</a>]</p>
     </div>
-    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-    <?php the_excerpt(); ?>
-    <p>[<a href="<?php the_permalink(); ?>">続きを読む</a>]</p>
-  </div>
-  <figure>
-  <?php if ( has_post_thumbnail() ): ?>
-    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'thumbnamil' ); ?></a>
-  <?php else: ?>
-    <a href="<?php the_permalink(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/common/noimage_180x180.png" height="180" width="180" alt=""></a>
-  <?php endif; ?>
-  </figure>
-</article><!-- /.news -->
+    <figure>
+    <?php if ( has_post_thumbnail() ): ?>
+      <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'thumbnamil' ); ?></a>
+    <?php else: ?>
+      <a href="<?php the_permalink(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/common/noimage_180x180.png" height="180" width="180" alt=""></a>
+    <?php endif; ?>
+    </figure>
+  </article><!-- /.news -->
 <?php
   endwhile;
 else : //記事がなかった場合
